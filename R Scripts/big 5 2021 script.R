2d8ca7e49467f78f8f22a344cec5272e9b55cbc8
robclark19 <robert.e.clark@wsu.edu>
2023-04-03 12:48:30 -0400

    Restructure R scripts.

--- R Scripts/big 5 2021 script.R
+++
@@ -1,8 +0,0 @@
-
-# Updated script for 2021-2022 "Big 5" manuscript prep
-# Code chunks taken from 2019 .R and .RMD files
-
-# ctrl shift c makes a comment
-# ctrl shift a makes everything space nicely
-# https://opendatascience.com/tips-and-tricks-in-rstudio-and-r-markdown/?utm_campaign=Learning%20Posts&utm_content=193372721&utm_medium=social&utm_source=twitter&hss_channel=tw-3018841323
-
